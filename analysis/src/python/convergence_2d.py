a044d52b1fa25085b206c85c34d75503e3a2d15e
Sai Meghana Surapaneni <saimeghana@Sais-MacBook-Air.local>
2023-10-05 14:55:53 -0400

    Adding Pythion 3 Conversions

--- analysis/src/python/convergence_2d.py
+++ analysis/src/python/convergence_2d.py
@@ -1,19 +1,34 @@
 import os
 from convergence import Errors2D
 
+# Define the directory where test results are stored
 testdir = '/media/shaheen/dev/results/maxwell_2d/convergence_averaged'
+
+# Define the base directory name prefix
 basedir = '_output_plane_'
+
+# Define the minimum and maximum values for 'basedir'
 basemin = 7
 basemax = 13
-frame   = 45
 
+# Define the frame number
+frame = 45
+
+# Define the directory where the summary will be saved
 savedir = '/simdesk/sandbox/emclaw/results/2D/convergence_averaged_test/summary'
 
-error = Errors2D(testdir,basedir,savedir,frame)
+# Create an instance of the Errors2D class
+error = Errors2D(testdir, basedir, savedir, frame)
 
-error.finesrc = os.path.join(testdir,basedir+'13')
+# Set the 'finesrc' attribute to the directory containing the highest 'basedir' value ('basedir13')
+error.finesrc = os.path.join(testdir, basedir + '13')
+
+# Set the 'basemin' and 'basemax' attributes
 error.basemin = basemin
 error.basemax = basemax
-error.debug   = True
 
+# Enable debugging mode
+error.debug = True
+
+# Call the 'convergence' method to perform convergence analysis
 error.convergence()
--- analysis/src/python/convergence_sin.py
+++ analysis/src/python/convergence_sin.py
@@ -1,28 +1,58 @@
 import os
 from convergence import Errors1D
 
+# Define the path to the directory containing MATLAB results
 matpath = '../matlab/results'
-matsrc  = '_sin_nc_65536.mat'
 
+# Define the MATLAB source file name
+matsrc = '_sin_nc_65536.mat'
+
+# Define the directory where 1D test results are stored
 testdir = '/simdesk/sandbox/emclaw/results/1D/_convergence_sin_homogeneous_tfluct'
+
+# Define the directory for comparison with another result
 compare_dir = '/simdesk/sandbox/emclaw/results/1D/_convergence_sin'
+
+# Define the base directory name prefix
 basedir = '_output_'
+
+# Define the minimum and maximum values for 'basedir'
 basemin = 7
 basemax = 15
-frame   = 5
 
-savedir = os.path.join(testdir,'_summaryb')
+# Define the frame number
+frame = 5
 
-error = Errors1D(testdir,basedir,savedir,frame)
+# Define the directory where the summary will be saved
+savedir = os.path.join(testdir, '_summaryb')
 
-error.matsrc  = os.path.join(matpath,matsrc)
-error.finesrc = os.path.join(testdir,basedir+'16')
+# Create an instance of the Errors1D class
+error = Errors1D(testdir, basedir, savedir, frame)
+
+# Set the 'matsrc' attribute to the path of the MATLAB source file
+error.matsrc = os.path.join(matpath, matsrc)
+
+# Set the 'finesrc' attribute to the directory containing 'basedir16'
+error.finesrc = os.path.join(testdir, basedir + '16')
+
+# Set the 'basemin' and 'basemax' attributes
 error.basemin = basemin
 error.basemax = basemax
-error.debug   = True
-error.p_line_range = [1,7]
+
+# Enable debugging mode
+error.debug = True
+
+# Define the range of P-line for analysis
+error.p_line_range = [1, 7]
+
+# Set the homogeneous flag to True
 error.homogeneous = True
+
+# Set the directory for comparison
 error.compare_dir = compare_dir
+
+# Enable comparison mode
 error.compare = True
 
+# Call the 'convergence' method to perform convergence analysis
 error.convergence()
